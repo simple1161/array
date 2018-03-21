dab54791831eb2e1b9aa82b2ef03bf821f13d336
simple1161 <varius06@mail.ru>
2018-03-21 21:39:28 +0300

    JavaDoc

---
+++ chapter_001/src/main/java/ru/simple/Calculate.java
@@ -0,0 +1,17 @@
+package ru.simple;
+
+/**
+* Class Класс для вычисления арефметических операций
+* @author semenov
+* @since 21.03.2018
+* @version 1
+*/
+public class Calculate{
+	/**
+	*Коструктор, вывода строки в консоль
+	*@param args - args.
+	*/
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
---
+++ chapter_001/src/main/java/ru/simple/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Semenov
+* @version 21.03.2018
+* @since 0.1
+*/
+package ru.simple;
