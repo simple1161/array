2280c180e44ea3ce73bfa8ed3b7f2958343ccbc8
simple1161 <varius06@mail.ru>
2018-03-30 23:14:05 +0300

    Расстояние междду двумя точками

---
+++ chapter_001/src/main/java/ru/simple/calculator/Point.java
@@ -0,0 +1,43 @@
+package ru.simple.calculator;
+
+/**
+ * Класс расчета расстояния между двумя точками
+ */
+
+public class Point {
+
+    private int x;
+
+    private int y;
+
+    public Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * Идеальный вес для мужщины.
+     * @return возвращает расстояние междду двумя точками.
+     */
+    public double distanceTo(Point that) {
+        Point a = this;
+        Point b = that;
+        int x1 = a.x;
+        int y1 = a.y;
+        int x2 = b.x;
+        int y2 = b.y;
+        return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+    }
+
+    public static void main(String[] args) {
+        Point a = new Point(0, 1);
+        Point b = new Point(2, 5);
+        System.out.println("x1 = " + a.x);
+        System.out.println("y1 = " + a.y);
+        System.out.println("x2 = " + b.x);
+        System.out.println("y2 = " + b.y);
+
+        double result = a.distanceTo(b);
+        System.out.println("Расстояние между точками А и В : " + result);
+    }
+}
