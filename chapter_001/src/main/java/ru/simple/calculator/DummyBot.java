119e182bc02a37f5ebd11afbb679da6861a55bf0
simple1161 <varius06@mail.ru>
2018-04-02 21:26:07 +0300

    Глупый бот.

---
+++ chapter_001/src/main/java/ru/simple/calculator/DummyBot.java
@@ -0,0 +1,24 @@
+package ru.simple.calculator;
+/**
+ * Class Глупый бот
+ * @author semenov
+ * @since 2.04.2018
+ * @version 1.0
+ */
+public class DummyBot {
+    /**
+     * Отвечает на вопросы.
+     * @param question Вопрос от клиента.
+     * @return Ответ.
+     */
+    public String answer(String question) {
+        String rsl = "Это ставит меня в тупик. Спросите другой вопрос.";
+
+        if("Привет, Бот.".equals(question)){
+            rsl = "Привет, умник.";
+        }else if("Пока.".equals(question)){
+            rsl = "До скорой встречи.";
+        }
+        return rsl;
+    }
+}
