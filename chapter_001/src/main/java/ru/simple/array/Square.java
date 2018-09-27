b42d03d31e058ae9af664d91fad7a9da5c5624ee
simple1161 <varius06@mail.ru>
2018-09-27 20:03:08 +0300

    6.0. Класс заполняет массив элементами возведенными в квадрат

---
+++ chapter_001/src/main/java/ru/simple/array/Square.java
@@ -0,0 +1,23 @@
+package ru.simple.array;
+/**
+ * Class 6.0 Класс заполняет массив числами от 1 до n возведенными в квадрат
+ * @author semenov
+ * @since 18.09.2018
+ * @version 1.0
+ */
+public class Square {
+    /**
+     * Метод принимающий положительное целое число n
+     * @param bound положительное целое число, ширина
+     * @return возвращает заполненый массив от 1 до n возведенными в квадрат
+     */
+    public int[] calculate(int bound) {
+        int[] rst = new int[bound];
+        int value = 1;
+        for (int i = 0; i < bound; i++) {
+            rst[i] = value * value;
+            value = value + 1;
+        }
+        return rst;
+    }
+}
