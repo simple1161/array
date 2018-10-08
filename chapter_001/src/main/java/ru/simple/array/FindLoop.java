f6a14d39a2aafcfb232bf000178466ffe632d07e
simple1161 <varius06@mail.ru>
2018-10-08 22:40:16 +0300

    6.1. Классический поиск перебором.

--- chapter_001/src/main/java/ru/simple/array/FindLoop.java
+++ chapter_001/src/main/java/ru/simple/array/FindLoop.java
@@ -3,7 +3,7 @@ package ru.simple.array;
 /**
  * Class 6.1 Класс переберает массив
  * @author semenov
- * @since 18.09.2018
+ * @since 08.10.2018
  * @version 1.0
  */
 public class FindLoop {
--- chapter_001/src/test/java/ru/simple/array/FindLoopTest.java
+++ chapter_001/src/test/java/ru/simple/array/FindLoopTest.java
@@ -4,6 +4,13 @@ import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
+/**
+ * Class 6.1 Тест для класса который переберает массив
+ * @author semenov
+ * @since 08.10.2018
+ * @version 1.0
+ */
+
 public class FindLoopTest {
 
     @Test
