575f88d288773cf203027480bb3bb5f059e7a233
Evgeny <varius06@mail.ru>
2018-10-09 19:33:16 +0300

    Class 6.2 Класс Переварачивает массив (версия 2)

--- chapter_001/src/main/java/ru/simple/array/Turn.java
+++ chapter_001/src/main/java/ru/simple/array/Turn.java
@@ -4,18 +4,15 @@ package ru.simple.array;
  * Class 6.2 Класс Переварачивает массив
  * @author semenov
  * @since 08.10.2018
- * @version 1.0
+ * @version 2.0
  */
 
 public class Turn {
     public int[] turn(int[] array) {
-        for (int i = 0; i < array.length; i++) {
-            int index = array.length - i - 1;
+        for (int i = 0; i < array.length / 2; i++) {
             int save = array[i];
-            array[i] = array[index];
-            array[index] = save;
-            if ((array.length / index)  == 2)
-                break;
+            array[i] = array[array.length - i - 1];
+            array[array.length - i - 1] = save;
         }
         return array;
     }
