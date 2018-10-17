8a76f8b809f923e1699f1f9f85d20a0b6e84a9da
Evgeny <varius06@mail.ru>
2018-10-17 19:50:23 +0300

    6.8. Удаление дубликатов в массиве. (Изменил сортировку)

--- chapter_001/src/main/java/ru/simple/array/ArrayDuplicate.java
+++ chapter_001/src/main/java/ru/simple/array/ArrayDuplicate.java
@@ -3,7 +3,7 @@ package ru.simple.array;
 import java.util.Arrays;
 
 /**
- * Class 6.8 Класс удаляет дубликаты в массиве.
+ * Class 6.7 Класс удаляет дубликаты в массиве.
  * @author semenov
  * @since 08.10.2018
  * @version 1.0
@@ -17,23 +17,17 @@ public class ArrayDuplicate {
      * @return возвращает массив без дубликатов.
      */
     public String[] remove(String[] array){
-        boolean check = false;
-        int i = 0;
-        for (; i < array.length; i++){
-            for (int j = i + 1; j < array.length; j++){
+        int value = array.length;
+        for (int i = 0; i < value; i++){
+            for (int j = i + 1; j < value; j++){
                 if(array[i].equals(array[j])){
-                    array[j] = array[array.length - 1];
-                    check = true;
-                    break;
+                    value--;
+                    array[j] = array[value];
+                    array[value] = array[i];
+                    j--;
                 }
-
-            }
-            if(check){
-                check = false;
-                i--;
-                array = Arrays.copyOf(array, array.length - 1);
             }
         }
-        return array;
+        return Arrays.copyOf(array, value);
     }
 }
