0761d8300f0a41f928e1d4a828fd467f41e41fc3
Evgeny <varius06@mail.ru>
2018-11-26 16:51:08 +0300

    6. Доработать тесты в StartUITest проверяющие вывод данных в консоль.

--- chapter_002/src/test/java/ru/simple/StartUITest.java
+++ chapter_002/src/test/java/ru/simple/StartUITest.java
@@ -1,5 +1,7 @@
 package ru.simple;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import ru.simple.models.Item;
 import ru.simple.start.Input;
@@ -7,10 +9,30 @@ import ru.simple.start.StartUI;
 import ru.simple.start.StubInput;
 import ru.simple.start.Tracker;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class StartUITest {
+    // поле содержит дефолтный вывод в консоль.
+    private final PrintStream stdout = System.out;
+    // буфер для результата.
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    @Before
+    public void loadOutput() {
+        System.out.println("execute before method");
+        System.setOut(new PrintStream(this.out));
+    }
+
+    @After
+    public void backOutput() {
+        System.setOut(this.stdout);
+        System.out.println("execute after method");
+    }
+
     @Test
     public void whenUserAddItem() {
         Tracker tracker = new Tracker();
@@ -80,5 +102,36 @@ public class StartUITest {
         Input input = new StubInput(new String[]{"5", previous.getName(), "6"});
         new StartUI(tracker, input).init();
         assertThat(tracker.findByName("test2")[0].getName(), is("test2"));
+    }
+    @Test
+    public void whenPrintUserFindName() {
+        Tracker tracker = new Tracker();
+        Item previous = new Item("test1", "testDescription");
+        tracker.add(previous);
+        Item next = new Item("test2", "testDescription2");
+        tracker.add(next);
+        Input input = new StubInput(new String[]{"5", previous.getName(), "6"});
+        new StartUI(tracker, input).init();
+        String[] sp = this.out.toString().split("\\r\\n");
+        assertThat(sp[8], is("Название заявки:" + " " + previous.getName() + "," + " Описание заявки:" + " " + previous.getDescription() + "," + " id заявки:" + " " + previous.getId()));
+    }
+
+    @Test
+    public void whenPrintUserShowAll() {
+        Tracker tracker = new Tracker();
+        Item previous = new Item("test1", "testDescription");
+        tracker.add(previous);
+        Item next = new Item("test2", "testDescription2");
+        tracker.add(next);
+        Input input = new StubInput(new String[]{"1", "6"});
+        new StartUI(tracker, input).init();
+        String[] sp = this.out.toString().split("\\r\\n");
+        String[] actual = new String[2];
+        actual[0] = sp[8];
+        actual[1] = sp[9];
+        String[] items = new String[2];
+        items[0] = "Название заявки:" + " " + previous.getName() + "," + " Описание заявки:" + " " + previous.getDescription() + "," + " id заявки:" + " " + previous.getId();
+        items[1] = "Название заявки:" + " " + next.getName() + "," + " Описание заявки:" + " " + next.getDescription() + "," + " id заявки:" + " " + next.getId();
+        assertThat(actual, is(items));
     }
 }
